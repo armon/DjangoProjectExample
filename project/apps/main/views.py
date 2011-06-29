89c33b592f403c3941b6ae5a06eacd7f76c33fe9
Armon Dadgar <armon.dadgar@gmail.com>
2011-06-28 20:09:25 -0700

    More concise view

--- project/apps/main/views.py
+++ project/apps/main/views.py
@@ -2,6 +2,5 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 def index(request):
-  return render_to_response("home/index.html", {'request':request}, 
-                            context_instance=RequestContext(request))
+  return render_to_response("home/index.html", context_instance=RequestContext(request))
   
